d0c4e825570e7f3073381552636586cc9315bc99
Romasssshka96 <Djydai@i.ua>
2024-11-04 16:53:45 +0300

    output to consol variables

---
+++ main.js
@@ -0,0 +1,63 @@
+let a= 'hello'
+console.log (a);
+
+let b= 'owu'
+console.log (b);
+
+let c= 'com'
+console.log (c);
+
+let d= 'ua'
+console.log (d);
+/*------------------------------*/
+
+let numOne= 1
+console.log(numOne);
+
+let numTwo= 10
+console.log(numTwo);
+
+let numThree= -999
+console.log(numThree);
+
+let numFour= 123
+console.log(numFour);
+
+const PI = 3.14
+console.log(PI);
+
+let numSix = 2.7
+console.log(parseFloat(numSix));
+/*parseFloat додав просто що б перед очами була, і запам'ятовувалась*/
+
+let numSeven= 16
+console.log(numSeven);
+
+/*-----------------------*/
+
+console.log(2>1);
+
+console.log(!!0);
+
+
+/*-----------------------------------------------*/
+
+
+let firstName = 'Умінський';
+let middleName ='Роман';
+let lastName ='Фрвнцович';
+
+let user = firstName + ' ' + middleName + ' ' + lastName;
+console.log(user);
+
+/*----------------------------------------------------*/
+
+
+let one = 100
+console.log(typeof one);
+
+let two = '100'
+console.log(typeof two);
+
+let three = true
+console.log(typeof three);
