901ede94f46b3cbbdfb7da123fe3482ca839bd6e
Romasssshka96 <Djydai@i.ua>
2024-11-08 18:38:23 +0300

    compleate hw3

--- hw2/hw2_12/main.js
+++ hw2/hw2_12/main.js
@@ -12,7 +12,7 @@ let coursesAndDurationArray = [
 if (coursesAndDurationArray[0].monthDuration > 5){
     console.log('Супер');
 }
-if (coursesAndDurationArray[1].monthDuration >5){
+if (coursesAndDurationArray[1].monthDuration > 5){
     console.log('Супер');
 }
 if (coursesAndDurationArray[2].monthDuration > 5){
@@ -24,6 +24,6 @@ if (coursesAndDurationArray[3].monthDuration >5){
 if (coursesAndDurationArray[4].monthDuration > 5){
     console.log('Супер');
 }
-if (coursesAndDurationArray[5].monthDuration >5){
+if (coursesAndDurationArray[5].monthDuration > 5){
     console.log('Супер')
 }
